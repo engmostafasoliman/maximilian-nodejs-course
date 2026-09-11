f5faff13686817605b31bd763f5ec93cbf3382ee
Mostafa Soliman <engmostafasoliman0@gmail.com>
2026-09-11 15:07:18 +0400

    262. Adding CSRF Protection

--- app.js
+++ app.js
@@ -9,10 +9,20 @@ const errorController = require("./controllers/errors");
 const mongoose = require("mongoose");
 const session = require("express-session");
 const MongoDBStore = require("connect-mongodb-session")(session);
+const cookieParser = require("cookie-parser");
+const { doubleCsrf } = require("csrf-csrf");
 // const Product = require("./models/products");
 const User = require("./models/user");
 const MONGODB_URI = 'mongodb+srv://devmostafasoliman_db_user:TvL2qEKQsoLuTR1a@cluster0.hqnkpd7.mongodb.net/shop?appName=Cluster0';
 const store = new MongoDBStore({uri:MONGODB_URI,collection:"sessions"});
+
+const { doubleCsrfProtection, generateCsrfToken } = doubleCsrf({
+    getSecret: () => "mysecretkey",
+    getSessionIdentifier: (req) => req.session.id,
+    cookieName: "csrf-token",
+    cookieOptions: { httpOnly: true, sameSite: "lax", secure: false },
+    getCsrfTokenFromRequest: (req) => req.body._csrf,
+});
 
 // const Cart = require("./models/cart");
 // const CartItem = require("./models/cat-item");
@@ -20,7 +30,9 @@ const store = new MongoDBStore({uri:MONGODB_URI,collection:"sessions"});
 // const OrderItem = require("./models/order-item");
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.static(path.join(__dirname,"public")));
-app.use(session({secret:"mysecretkey",resave:false,saveUninitialized:false,store:store}));
+app.use(cookieParser("mysecretkey"));
+app.use(session({secret:"mysecretkey",resave:false,saveUninitialized:true,store:store}));
+app.use(doubleCsrfProtection);
 
 app.set("view engine","ejs");
 app.set("views","views");
@@ -39,6 +51,7 @@ app.use((req,res,next)=>{
 
 app.use((req,res,next)=>{
     res.locals.isAuthenticated = req.session.isLoggedIn;
+    res.locals.csrfToken = generateCsrfToken(req, res);
     next();
 });
 
